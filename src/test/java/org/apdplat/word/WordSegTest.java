9ba801c974242d56422a7e7946e1ff1d3c9a3304
ysc <yang-shangchuan@qq.com>
2014-03-21 04:04:10 +0800

    增加测试用例

--- src/test/java/org/apdplat/word/WordSegTest.java
+++ src/test/java/org/apdplat/word/WordSegTest.java
@@ -23,9 +23,37 @@ public class WordSegTest {
         assertEquals(expResult, result.toString());
     }
     @Test
+    public void testSeg2() {
+        String text = "杨";
+        String expResult = "[杨]";
+        List<String> result = WordSeg.seg(text);
+        assertEquals(expResult, result.toString());
+    }
+    @Test
+    public void testSeg3() {
+        String text = "杨尚川好";
+        String expResult = "[杨尚川, 好]";
+        List<String> result = WordSeg.seg(text);
+        assertEquals(expResult, result.toString());
+    }
+    @Test
     public void testSegReverse() {
         String text = "长春市长春节致辞";
         String expResult = "[长春, 市长, 春节, 致辞]";
+        List<String> result = WordSeg.segReverse(text);
+        assertEquals(expResult, result.toString());
+    }
+    @Test
+    public void testSegReverse2() {
+        String text = "好";
+        String expResult = "[好]";
+        List<String> result = WordSeg.segReverse(text);
+        assertEquals(expResult, result.toString());
+    }
+    @Test
+    public void testSegReverse3() {
+        String text = "杨尚川好";
+        String expResult = "[杨尚川, 好]";
         List<String> result = WordSeg.segReverse(text);
         assertEquals(expResult, result.toString());
     }    
