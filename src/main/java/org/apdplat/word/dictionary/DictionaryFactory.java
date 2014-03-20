803f6ff1b8cb959f8029d59a00b761dc610582cf
ysc <yang-shangchuan@qq.com>
2014-03-20 22:59:43 +0800

    格式化输出统计词长分布

--- src/main/java/org/apdplat/word/dictionary/DictionaryFactory.java
+++ src/main/java/org/apdplat/word/dictionary/DictionaryFactory.java
@@ -24,10 +24,10 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 
 /**
  * 词典工厂
@@ -63,7 +63,7 @@ public final class DictionaryFactory {
                 //统计平均词长
                 int totalLength=0;
                 //统计词长分布
-                Map<Integer,Integer> map = new HashMap<>();
+                Map<Integer,Integer> map = new TreeMap<>();
                 //这行代码够简洁
                 List<String> lines = Files.readAllLines(Paths.get(dicPath), Charset.forName("utf-8"));
                 //采用迭代器方式遍历，一边建Trie一边释放词典数据，以防止内存不够
@@ -89,6 +89,13 @@ public final class DictionaryFactory {
                 long cost = System.currentTimeMillis() - start;
                 System.out.println("完成初始化词典，耗时"+cost+" 毫秒，词数目："+wordCount);
                 System.out.println("词典最大词长："+DIC.getMaxLength());
+                for(int len : map.keySet()){
+                    if(len<10){
+                        System.out.println("词长  "+len+" 的词数为："+map.get(len));
+                    }else{
+                        System.out.println("词长 "+len+" 的词数为："+map.get(len));
+                    }
+                }
                 System.out.println("词典平均词长："+(float)totalLength/wordCount);
             } catch (IOException | ClassNotFoundException | IllegalAccessException | InstantiationException ex) {
                 System.err.println("词典装载失败:"+ex.getMessage());
