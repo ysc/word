162671dd7c8c79e08af9998d965166d7eb5644f1
ysc <yang-shangchuan@qq.com>
2014-03-22 13:08:34 +0800

    中文分词使用方法

--- src/main/java/org/apdplat/word/WordSeg.java
+++ src/main/java/org/apdplat/word/WordSeg.java
@@ -22,18 +22,14 @@ package org.apdplat.word;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Stack;
-import org.apdplat.word.dictionary.Dictionary;
-import org.apdplat.word.dictionary.DictionaryFactory;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
+import org.apdplat.word.segmentation.SegmentationFactory;
 
 /**
- * 基于词典的正向最大匹配算法
- * 基于词典的逆向最大匹配算法
+ * 中文分词使用方法
  * @author 杨尚川
  */
-public class WordSeg {
-    private static final Dictionary DIC = DictionaryFactory.getDictionary();
-    
+public class WordSeg {    
     public static void main(String[] args){
         long start = System.currentTimeMillis();
         List<String> sentences = new ArrayList<>();
@@ -66,94 +62,10 @@ public class WordSeg {
         sentences.add("把手举起来");
         sentences.add("茶杯的把手断了");
         for(String sentence : sentences){
-            System.out.println("正向最大匹配: "+seg(sentence));
-            System.out.println("逆向最大匹配: "+segReverse(sentence));
+            System.out.println("正向最大匹配: "+SegmentationFactory.getSegmentation(SegmentationAlgorithm.MaximumMatching).seg(sentence));
+            System.out.println("逆向最大匹配: "+SegmentationFactory.getSegmentation(SegmentationAlgorithm.ReverseMaximumMatching).seg(sentence));
         }
         long cost = System.currentTimeMillis() - start;
         System.out.println("cost: "+cost);
-    }
-    public static List<String> seg(String text){        
-        List<String> result = new ArrayList<>();
-        //文本长度
-        final int textLen=text.length();
-        //从未分词的文本中截取的长度
-        int len=DIC.getMaxLength();
-        //剩下未分词的文本的索引
-        int start=0;
-        //只要有词未切分完就一直继续
-        while(start<textLen){
-            if(len>textLen-start){
-                //如果未分词的文本的长度小于截取的长度
-                //则缩短截取的长度
-                len=textLen-start;
-            }
-            //用长为len的字符串查词典
-            while(!DIC.contains(text, start, len)){
-                //如果长度为一且在词典中未找到匹配
-                //则按长度为一切分
-                if(len==1){
-                    break;
-                }
-                //如果查不到，则长度减一后继续
-                len--;
-            }
-            result.add(text.substring(start, start+len));
-            //从待分词文本中向后移动索引，滑过已经分词的文本
-            start+=len;
-            //每一次成功切词后都要重置截取长度
-            len=DIC.getMaxLength();
-        }
-        return result;
-    }
-    public static List<String> segReverse(String text){        
-        Stack<String> result = new Stack<>();
-        //文本长度
-        final int textLen=text.length();
-        //从未分词的文本中截取的长度
-        int len=DIC.getMaxLength();
-        //剩下未分词的文本的索引
-        int start=textLen-len;
-        //处理文本长度小于最大词长的情况
-        if(start<0){
-            start=0;
-        }
-        if(len>textLen-start){
-            //如果未分词的文本的长度小于截取的长度
-            //则缩短截取的长度
-            len=textLen-start;
-        }
-        //只要有词未切分完就一直继续
-        while(start>=0 && len>0){
-            //用长为len的字符串查词典
-            while(!DIC.contains(text, start, len)){
-                //如果长度为一且在词典中未找到匹配
-                //则按长度为一切分
-                if(len==1){
-                    break;
-                }
-                //如果查不到，则长度减一
-                //索引向后移动一个字，然后继续
-                len--;
-                start++;
-            }
-            result.push(text.substring(start, start+len));
-            //每一次成功切词后都要重置截取长度
-            len=DIC.getMaxLength();            
-            if(len>start){
-                //如果未分词的文本的长度小于截取的长度
-                //则缩短截取的长度
-                len=start;
-            }
-            //每一次成功切词后都要重置开始索引位置
-            //从待分词文本中向前移动最大词长个索引
-            //将未分词的文本纳入下次分词的范围
-            start-=len;
-        }
-        len=result.size();
-        List<String> list = new ArrayList<>(len);
-        for(int i=0;i<len;i++){
-            list.add(result.pop());
-        }
-        return list;
-    }
+    }    
 }
