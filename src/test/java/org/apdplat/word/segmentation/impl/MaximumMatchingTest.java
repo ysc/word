84b7a66358cfe8dc4b085a3661ea18fce1fb8ad0
ysc <yang-shangchuan@qq.com>
2014-03-22 13:05:18 +0800

    重构单元测试代码

--- src/test/java/org/apdplat/word/segmentation/impl/MaximumMatchingTest.java
+++ src/test/java/org/apdplat/word/segmentation/impl/MaximumMatchingTest.java
@@ -20,10 +20,10 @@
 
 package org.apdplat.word.segmentation.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.apdplat.word.segmentation.Segmentation;
 import org.apdplat.word.segmentation.Word;
-import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -32,30 +32,22 @@ import static org.junit.Assert.*;
  * @author 杨尚川
  */
 public class MaximumMatchingTest {
-    private Segmentation segmentation = null;
-    @Before
-    public void setUp() {
-        segmentation = new MaximumMatching();
-    }
     @Test
-    public void testSeg1() {
-        String text = "长春市长春节致辞";
-        String expResult = "[长春市, 长春, 节, 致辞]";
-        List<Word> result = segmentation.seg(text);
-        assertEquals(expResult, result.toString());
-    }
-    @Test
-    public void testSeg2() {
-        String text = "杨";
-        String expResult = "[杨]";
-        List<Word> result = segmentation.seg(text);
-        assertEquals(expResult, result.toString());
-    }
-    @Test
-    public void testSeg3() {
-        String text = "杨尚川好";
-        String expResult = "[杨尚川, 好]";
-        List<Word> result = segmentation.seg(text);
-        assertEquals(expResult, result.toString());
+    public void testSeg() {
+        Segmentation segmentation = new MaximumMatching();
+        List<String> text = new ArrayList<>();
+        text.add("长春市长春节致辞");
+        text.add("杨");
+        text.add("杨尚川好");
+        
+        List<String> expResult = new ArrayList<>();
+        expResult.add("[长春市, 长春, 节, 致辞]");
+        expResult.add("[杨]");
+        expResult.add("[杨尚川, 好]");
+        
+        for(int i=0; i<text.size(); i++){
+            List<Word> result = segmentation.seg(text.get(i));
+            assertEquals(expResult.get(i).toString(), result.toString());
+        }
     }
 }
--- src/test/java/org/apdplat/word/segmentation/impl/ReverseMaximumMatchingTest.java
+++ src/test/java/org/apdplat/word/segmentation/impl/ReverseMaximumMatchingTest.java
@@ -20,11 +20,11 @@
 
 package org.apdplat.word.segmentation.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import org.apdplat.word.segmentation.Segmentation;
 import org.apdplat.word.segmentation.Word;
 import static org.junit.Assert.assertEquals;
-import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -32,30 +32,22 @@ import org.junit.Test;
  * @author 杨尚川
  */
 public class ReverseMaximumMatchingTest {    
-    private Segmentation segmentation = null;
-    @Before
-    public void setUp() {
-        segmentation = new ReverseMaximumMatching();
-    }    
     @Test
-    public void testSegReverse() {
-        String text = "长春市长春节致辞";
-        String expResult = "[长春, 市长, 春节, 致辞]";
-        List<Word> result = segmentation.seg(text);
-        assertEquals(expResult, result.toString());
-    }
-    @Test
-    public void testSegReverse2() {
-        String text = "好";
-        String expResult = "[好]";
-        List<Word> result = segmentation.seg(text);
-        assertEquals(expResult, result.toString());
-    }
-    @Test
-    public void testSegReverse3() {
-        String text = "杨尚川好";
-        String expResult = "[杨尚川, 好]";
-        List<Word> result = segmentation.seg(text);
-        assertEquals(expResult, result.toString());
+    public void testSeg() {
+        Segmentation segmentation = new ReverseMaximumMatching();
+        List<String> text = new ArrayList<>();
+        text.add("长春市长春节致辞");
+        text.add("好");
+        text.add("杨尚川好");
+        
+        List<String> expResult = new ArrayList<>();
+        expResult.add("[长春, 市长, 春节, 致辞]");
+        expResult.add("[好]");
+        expResult.add("[杨尚川, 好]");
+        
+        for(int i=0; i<text.size(); i++){
+            List<Word> result = segmentation.seg(text.get(i));
+            assertEquals(expResult.get(i).toString(), result.toString());
+        }
     }
 }
