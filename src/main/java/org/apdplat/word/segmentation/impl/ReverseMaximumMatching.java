995313a173a46398db196a4907f1e3f3a215e9b1
ysc <yang-shangchuan@qq.com>
2014-03-22 12:49:01 +0800

    基于词典的逆向最大匹配算法，实现新的分词接口

---
+++ src/main/java/org/apdplat/word/segmentation/impl/ReverseMaximumMatching.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.word.segmentation.impl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+import org.apdplat.word.dictionary.Dictionary;
+import org.apdplat.word.dictionary.DictionaryFactory;
+import org.apdplat.word.segmentation.Segmentation;
+import org.apdplat.word.segmentation.Word;
+
+/**
+ * 基于词典的逆向最大匹配算法
+ * Dictionary-based reverse maximum matching algorithm
+ * @author 杨尚川
+ */
+public class ReverseMaximumMatching implements Segmentation{
+    private static final Dictionary DIC = DictionaryFactory.getDictionary();
+    @Override
+    public List<Word> seg(String text) {
+        Stack<Word> result = new Stack<>();
+        //文本长度
+        final int textLen=text.length();
+        //从未分词的文本中截取的长度
+        int len=DIC.getMaxLength();
+        //剩下未分词的文本的索引
+        int start=textLen-len;
+        //处理文本长度小于最大词长的情况
+        if(start<0){
+            start=0;
+        }
+        if(len>textLen-start){
+            //如果未分词的文本的长度小于截取的长度
+            //则缩短截取的长度
+            len=textLen-start;
+        }
+        //只要有词未切分完就一直继续
+        while(start>=0 && len>0){
+            //用长为len的字符串查词典
+            while(!DIC.contains(text, start, len)){
+                //如果长度为一且在词典中未找到匹配
+                //则按长度为一切分
+                if(len==1){
+                    break;
+                }
+                //如果查不到，则长度减一
+                //索引向后移动一个字，然后继续
+                len--;
+                start++;
+            }
+            result.push(new Word(text.substring(start, start+len)));
+            //每一次成功切词后都要重置截取长度
+            len=DIC.getMaxLength();            
+            if(len>start){
+                //如果未分词的文本的长度小于截取的长度
+                //则缩短截取的长度
+                len=start;
+            }
+            //每一次成功切词后都要重置开始索引位置
+            //从待分词文本中向前移动最大词长个索引
+            //将未分词的文本纳入下次分词的范围
+            start-=len;
+        }
+        len=result.size();
+        List<Word> list = new ArrayList<>(len);
+        for(int i=0;i<len;i++){
+            list.add(result.pop());
+        }
+        return list;        
+    }
+}
