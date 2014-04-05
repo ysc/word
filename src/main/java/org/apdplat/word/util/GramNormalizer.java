e3f9c15198abf3166b5537bdb15a48b7b200920d
ysc <yang-shangchuan@qq.com>
2014-04-05 14:18:42 +0800

    对N元模型的词频进行归一化处理

--- src/main/java/org/apdplat/word/util/GramNormalizer.java
+++ src/main/java/org/apdplat/word/util/GramNormalizer.java
@@ -24,12 +24,17 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
  * N元模型规范化
+ * 对N元模型的词频进行归一化处理
  * 去除N元模型中包含非中文字符的记录
  * @author 杨尚川
  */
@@ -37,10 +42,85 @@ public class GramNormalizer {
     public static void main(String[] args) throws IOException{
         String src = "src/main/resources/bigram.txt";
         String dst = "src/main/resources/bigram.txt";
+        uniform(src, dst, 2);
         norm(src, dst, 2);
         src = "src/main/resources/trigram.txt";
         dst = "src/main/resources/trigram.txt";
+        uniform(src, dst, 3);
         norm(src, dst, 3);
+    }
+    /**
+     * 对N元模型的词频进行归一化处理
+     * @param src 模型输入路径
+     * @param dst 模型保存路径
+     * @param n 几元模型
+     * @throws IOException 
+     */
+    public static void uniform(String src, String dst, int n) throws IOException{
+        List<String> lines = Files.readAllLines(Paths.get(src), Charset.forName("utf-8"));
+        Map<String, Float> map = new HashMap<>();
+        Map<String, Float> counts = new HashMap<>();
+        //统计n-1前缀完全相同的总数
+        for(String line : lines){
+            String[] attr = line.split(" -> ");
+            if(attr == null || attr.length != 2){
+                System.out.println("错误数据："+line);
+                continue;
+            }
+            String key = attr[0];
+            String value = attr[1];
+            String[] words = key.split(":");
+            if(words == null || words.length != n || value == null){
+                System.out.println("错误数据："+line);
+                continue;
+            }
+            if(value.indexOf(".") != -1){
+                System.out.println("已经做过归一化处理，忽略...");
+                //不用处理了，程序返回
+                return ;
+            }
+            Float frequence = Float.parseFloat(value);
+            map.put(key, frequence);
+            String base = getBase(words);
+            Float count = counts.get(base);
+            if(count == null){
+                count = frequence;
+            }else{
+                count += frequence;
+            }
+            counts.put(base, count);
+        }
+        //对n-1前缀相同的情况重新计算分值
+        for(String key : map.keySet()){
+            String base = getBase(key.split(":"));
+            Float count = counts.get(base);
+            Float value = map.get(key);
+            Float score = value/count;
+            map.put(key, score);
+        }
+        counts.clear();
+        List<String> list = new ArrayList<>(map.size());
+        //把map转换为list
+        for(String key : map.keySet()){
+            list.add(key+" -> "+map.get(key));
+        }
+        map.clear();
+        //排序
+        Collections.sort(list);
+        System.out.println("总的模型数："+list.size());
+        Files.write(Paths.get(dst), list, Charset.forName("utf-8"));
+    }
+    /**
+     * 获取N元模型的N-1前缀
+     * @param words N元模型
+     * @return N-1前缀
+     */
+    private static String getBase(String[] words) {
+        StringBuilder base = new StringBuilder();
+        for(int i=0; i<words.length-1; i++){
+            base.append(words[i]);
+        }
+        return base.toString();
     }
     /**
      * 去除N元模型中包含非中文字符的记录
