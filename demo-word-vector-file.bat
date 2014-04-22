7f3af7075e004a6c42d6a03e23b65e48e202c447
ysc <yang-shangchuan@qq.com>
2014-04-23 07:19:23 +0800

    重构SegFile类名为WordSegmenter

--- demo-word-vector-file.bat
+++ demo-word-vector-file.bat
@@ -8,7 +8,7 @@ set JAVA_OPTS=-Xms12000m -Xmx12000m
 set CLASS_PATH=target/classes;target/dependency/slf4j-api-1.6.4.jar;target/dependency/logback-classic-0.9.28.jar;target/dependency/logback-core-0.9.28.jar
 set EXECUTOR=java %JAVA_OPTS% -cp %CLASS_PATH%
 rem 1、对文件%TEXT_FILE%进行分词，词之间以空格分隔，内容保存到文件target/word.txt
-call %EXECUTOR% org.apdplat.word.SegFile %TEXT_FILE% target/word.txt
+call %EXECUTOR% org.apdplat.word.WordSegmenter file %TEXT_FILE% target/word.txt
 rem 2、对分好词的文件target/word.txt建立词向量，将词向量保存到文件target/vector.txt，将词汇表保存到文件target/vocabulary.txt
 rem 上下文长度为
 set CONTEXT_WINDOW_LENGTH=2
