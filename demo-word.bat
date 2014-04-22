40493ac996cf0aeba13b9bca23e3957630e38075
ysc <yang-shangchuan@qq.com>
2014-04-23 07:18:46 +0800

    重构WordSeg类名为WordSegmenter

--- demo-word.bat
+++ demo-word.bat
@@ -3,4 +3,4 @@ call mvn clean install dependency:copy-dependencies
 set JAVA_OPTS=-Xms1200m -Xmx1200m
 set CLASS_PATH=target/classes;target/dependency/slf4j-api-1.6.4.jar;target/dependency/logback-classic-0.9.28.jar;target/dependency/logback-core-0.9.28.jar
 set EXECUTOR=java %JAVA_OPTS% -cp %CLASS_PATH%
-call %EXECUTOR% org.apdplat.word.WordSeg gbk
+call %EXECUTOR% org.apdplat.word.WordSegmenter gbk
