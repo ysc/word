8ab7b444f38336248d78409d77d1e810a853aa7e
ysc <yang-shangchuan@qq.com>
2014-04-24 05:59:22 +0800

    分词算法抽象类

---
+++ src/main/java/org/apdplat/word/segmentation/impl/AbstractSegmentation.java
@@ -0,0 +1,90 @@
+/**
+ * 
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package org.apdplat.word.segmentation.impl;
+
+import java.util.List;
+import java.util.Stack;
+import org.apdplat.word.dictionary.Dictionary;
+import org.apdplat.word.dictionary.DictionaryFactory;
+import org.apdplat.word.segmentation.Segmentation;
+import org.apdplat.word.segmentation.Word;
+import org.apdplat.word.util.WordConfTools;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 分词算法抽象类
+ * @author 杨尚川
+ */
+public abstract class AbstractSegmentation  implements Segmentation{
+    protected static final Logger LOGGER = LoggerFactory.getLogger(AbstractSegmentation.class);
+    protected static final Dictionary DIC = DictionaryFactory.getDictionary();
+    protected static final boolean PERSON_NAME_RECOGNIZE = "true".equals(WordConfTools.get("person.name.recognize", "true"));
+    protected static final boolean KEEP_WHITESPACE = "true".equals(WordConfTools.get("keep.whitespace", "false"));
+
+    protected void addWord(List<Word> result, String text, int start, int len){
+        //方便编译器优化
+        if(KEEP_WHITESPACE){
+            //保留空白字符
+            result.add(new Word(text.substring(start, start+len).toLowerCase()));
+        }else{
+            //忽略空白字符，包括：空格、全角空格、\t、\n                
+            if(len > 1){
+                //长度大于1，不会是空白字符
+                result.add(new Word(text.substring(start, start+len).toLowerCase()));
+            }else{
+                //长度为1，只要非空白字符
+                if(!(text.substring(start, start+len).charAt(0) == ' ')
+                    && !(text.substring(start, start+len).charAt(0) == '　')
+                    && !(text.substring(start, start+len).charAt(0) == '\t')
+                    && !(text.substring(start, start+len).charAt(0) == '\n')){
+                    //不是空白字符，保留
+                    result.add(new Word(text.substring(start, start+len).toLowerCase()));                        
+                }
+            }
+        }
+    }
+    protected void addWord(Stack<Word> result, String text, int start, int len){
+        //方便编译器优化
+        if(KEEP_WHITESPACE){
+            //保留空白字符
+            result.push(new Word(text.substring(start, start+len).toLowerCase()));
+        }else{
+            //忽略空白字符，包括：空格、全角空格、\t、\n                
+            if(len > 1){
+                //长度大于1，不会是空白字符
+                result.push(new Word(text.substring(start, start+len).toLowerCase()));
+            }else{
+                //长度为1，只要非空白字符
+                if(!(text.substring(start, start+len).charAt(0) == ' ')
+                    && !(text.substring(start, start+len).charAt(0) == '　')
+                    && !(text.substring(start, start+len).charAt(0) == '\t')
+                    && !(text.substring(start, start+len).charAt(0) == '\n')){
+                    //不是空白字符，保留
+                    result.push(new Word(text.substring(start, start+len).toLowerCase()));                        
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+
+    }
+}
