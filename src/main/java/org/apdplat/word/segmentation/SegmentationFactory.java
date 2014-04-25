d9b149e5917d223323c4dced665a014c1473ae26
ysc <yang-shangchuan@qq.com>
2014-04-25 20:40:42 +0800

    分词工厂使用对象缓存池

--- src/main/java/org/apdplat/word/segmentation/SegmentationFactory.java
+++ src/main/java/org/apdplat/word/segmentation/SegmentationFactory.java
@@ -20,6 +20,8 @@
 
 package org.apdplat.word.segmentation;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -32,15 +34,20 @@ import org.slf4j.LoggerFactory;
  */
 public class SegmentationFactory {
     private static final Logger LOGGER = LoggerFactory.getLogger(SegmentationFactory.class);
+    private static final Map<String, Segmentation> pool = new HashMap<>();
     private SegmentationFactory(){};
-    public static Segmentation getSegmentation(SegmentationAlgorithm segmentationAlgorithm){
+    public static synchronized Segmentation getSegmentation(SegmentationAlgorithm segmentationAlgorithm){
         String clazz = "org.apdplat.word.segmentation.impl."+segmentationAlgorithm.name();
-        LOGGER.info("分词实现类："+clazz);
-        try {
-            return (Segmentation)Class.forName(clazz).newInstance();
-        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
-            LOGGER.info("构造分词实现类失败："+ex.getMessage());
+        Segmentation segmentation = pool.get(clazz);
+        if(segmentation == null){
+            LOGGER.info("构造分词实现类："+clazz);
+            try {
+                segmentation = (Segmentation)Class.forName(clazz).newInstance();
+                pool.put(clazz, segmentation);
+            } catch (ClassNotFoundException | InstantiationException | IllegalAccessException ex) {
+                LOGGER.info("构造分词实现类失败：", ex);
+            }
         }
-        return null;
+        return segmentation;
     }
 }
