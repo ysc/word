ba07a404b1b7a2d62374e9e72aa956c3d8f8a9a0
ysc <yang-shangchuan@qq.com>
2014-04-25 20:52:48 +0800

    多种分词算法效果评估

--- src/main/java/org/apdplat/word/corpus/Evaluation.java
+++ src/main/java/org/apdplat/word/corpus/Evaluation.java
@@ -31,9 +31,12 @@ import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.apdplat.word.WordSegmenter;
 import org.apdplat.word.recognition.Punctuation;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
 import org.slf4j.LoggerFactory;
 
 /**
@@ -48,9 +51,9 @@ public class Evaluation {
         String corpusText = "target/evaluation/corpus-text.txt";
         String testText = "target/evaluation/test-text.txt";
         String standardText = "target/evaluation/standard-text.txt";
-        String resultText = "target/evaluation/result-text.txt";
-        String perfectResult = "target/evaluation/perfect-result.txt";
-        String wrongResult = "target/evaluation/wrong-result.txt";
+        String resultText = "target/evaluation/result-text-";
+        String perfectResult = "target/evaluation/perfect-result-";
+        String wrongResult = "target/evaluation/wrong-result-";
         Path path = Paths.get("target/evaluation");
         if(!Files.exists(path)){
             Files.createDirectory(path);
@@ -59,10 +62,21 @@ public class Evaluation {
         ExtractText.extractFromCorpus(corpusText, " ");
         //2、生成测试数据集和标准数据集
         generateDataset(corpusText, testText, standardText);
-        //3、对测试数据集进行分词
-        WordSegmenter.segWithStopWords(new File(testText), new File(resultText));
-        //4、分词效果评估
-        evaluation(resultText, standardText, perfectResult, wrongResult);
+        Map<String, String> map = new HashMap<>();
+        for(SegmentationAlgorithm segmentationAlgorithm : SegmentationAlgorithm.values()){
+            //3、对测试数据集进行分词
+            WordSegmenter.segWithStopWords(new File(testText), new File(resultText+segmentationAlgorithm.name()+".txt"), segmentationAlgorithm);
+            //4、分词效果评估
+            String report = evaluation(resultText+segmentationAlgorithm.name()+".txt", standardText, perfectResult+segmentationAlgorithm.name()+".txt", wrongResult+segmentationAlgorithm.name()+".txt");
+            map.put(segmentationAlgorithm.name(), report);
+        }
+        //5、输出测试报告
+        LOGGER.info("*********************************************");
+        for(String key : map.keySet()){
+            LOGGER.info(key+" ：");
+            LOGGER.info(map.get(key));
+        }
+        LOGGER.info("*********************************************");
     }
     /**
      * 生成测试数据集和标准数据集
@@ -94,7 +108,7 @@ public class Evaluation {
      * @param resultText 实际分词结果
      * @param standardText 标准分词结果
      */
-    private static void evaluation(String resultText, String standardText, String perfectResult, String wrongResult) {
+    private static String evaluation(String resultText, String standardText, String perfectResult, String wrongResult) {
         long start = System.currentTimeMillis();
         int perfectCount=0;
         int wrongCount=0;
@@ -124,9 +138,9 @@ public class Evaluation {
             LOGGER.error("分词效果评估失败：", ex);
         }
         long cost = System.currentTimeMillis() - start;
-        LOGGER.info("耗时："+cost+" 毫秒");
-        LOGGER.info("完美数："+perfectCount+" ,错误数："+wrongCount);
         int total = perfectCount+wrongCount;
-        LOGGER.info("完美率："+perfectCount/(float)total*100+"% ,错误率："+wrongCount/(float)total*100+"%");
+        String report = "耗时："+cost+" 毫秒 ，总行数："+total+" ，完美行数："+perfectCount+" ，错误行数："+wrongCount+" ，完美率："+perfectCount/(float)total*100+"% ，错误率："+wrongCount/(float)total*100+"%";
+        LOGGER.info(report);
+        return report;
     }
 }
