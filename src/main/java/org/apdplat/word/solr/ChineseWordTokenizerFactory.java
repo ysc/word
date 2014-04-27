7767b179efb8af93b312be89abd278673e0ef91a
ysc <yang-shangchuan@qq.com>
2014-04-28 01:38:05 +0800

    Solr插件可以通过segAlgorithm和conf参数来指定分词算法和配置文件路径

--- src/main/java/org/apdplat/word/solr/ChineseWordTokenizerFactory.java
+++ src/main/java/org/apdplat/word/solr/ChineseWordTokenizerFactory.java
@@ -26,17 +26,54 @@ import org.apache.lucene.analysis.Tokenizer;
 import org.apache.lucene.analysis.util.TokenizerFactory;
 import org.apache.lucene.util.AttributeSource;
 import org.apdplat.word.lucene.ChineseWordTokenizer;
+import org.apdplat.word.segmentation.Segmentation;
+import org.apdplat.word.segmentation.SegmentationAlgorithm;
+import org.apdplat.word.segmentation.SegmentationFactory;
+import org.apdplat.word.util.WordConfTools;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Lucene中文分词器工厂
  * @author 杨尚川
  */
 public class ChineseWordTokenizerFactory extends TokenizerFactory {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ChineseWordTokenizerFactory.class);
+    private Segmentation segmentation = null;
     public ChineseWordTokenizerFactory(Map<String, String> args){
         super(args);
+        if(args != null){
+            String conf = args.get("conf");
+            if(conf != null){
+                //强制覆盖默认配置
+                WordConfTools.forceOverride(conf);
+            }else{
+                LOGGER.info("没有指定conf参数");
+            }
+            String algorithm = args.get("segAlgorithm");
+            if(algorithm != null){
+                try{
+                    SegmentationAlgorithm segmentationAlgorithm = SegmentationAlgorithm.valueOf(algorithm);
+                    segmentation = SegmentationFactory.getSegmentation(segmentationAlgorithm);
+                    LOGGER.info("使用指定分词算法："+algorithm);
+                }catch(Exception e){
+                    LOGGER.error("参数segAlgorithm指定的值错误："+algorithm);
+                    LOGGER.error("参数segAlgorithm可指定的值有：");
+                    for(SegmentationAlgorithm sa : SegmentationAlgorithm.values()){
+                        LOGGER.error("\t"+sa.name());
+                    }
+                }
+            }else{
+                LOGGER.info("没有指定segAlgorithm参数");
+            }
+        }
+        if(segmentation == null){
+            segmentation = SegmentationFactory.getSegmentation(SegmentationAlgorithm.BidirectionalMaximumMatching);
+            LOGGER.info("使用默认分词算法："+SegmentationAlgorithm.BidirectionalMaximumMatching);
+        }
     }
     @Override
     public Tokenizer create(AttributeSource.AttributeFactory af, Reader reader) {
-        return new ChineseWordTokenizer(reader);
+        return new ChineseWordTokenizer(reader, segmentation);
     }
 }
