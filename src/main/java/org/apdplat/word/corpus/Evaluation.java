03a0e1dd4e26fcb483d997d861a2a8b74440e606
ysc <yang-shangchuan@qq.com>
2014-04-29 07:43:50 +0800

    改进分词效果评估报告

--- src/main/java/org/apdplat/word/corpus/Evaluation.java
+++ src/main/java/org/apdplat/word/corpus/Evaluation.java
@@ -31,10 +31,10 @@ import java.io.OutputStreamWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import org.apdplat.word.WordSegmenter;
 import org.apdplat.word.recognition.Punctuation;
@@ -63,30 +63,40 @@ public class Evaluation {
         //1、抽取文本
         ExtractText.extractFromCorpus(corpusText, " ", false);
         //2、生成测试数据集和标准数据集
-        generateDataset(corpusText, testText, standardText);
-        Map<String, String> map = new HashMap<>();
+        int textCharCount = generateDataset(corpusText, testText, standardText);
+        List<EvaluationResult> result = new ArrayList<>();
         for(SegmentationAlgorithm segmentationAlgorithm : SegmentationAlgorithm.values()){
+            long start = System.currentTimeMillis();
             //3、对测试数据集进行分词
             WordSegmenter.segWithStopWords(new File(testText), new File(resultText+segmentationAlgorithm.name()+".txt"), segmentationAlgorithm);
+            long cost = System.currentTimeMillis() - start;
+            float rate = textCharCount/(float)cost;
             //4、分词效果评估
-            String report = evaluation(resultText+segmentationAlgorithm.name()+".txt", standardText, perfectResult+segmentationAlgorithm.name()+".txt", wrongResult+segmentationAlgorithm.name()+".txt");
-            map.put(segmentationAlgorithm.name(), report);
+            EvaluationResult evaluationResult = evaluation(resultText+segmentationAlgorithm.name()+".txt", standardText, perfectResult+segmentationAlgorithm.name()+".txt", wrongResult+segmentationAlgorithm.name()+".txt");
+            evaluationResult.setSegmentationAlgorithm(segmentationAlgorithm);
+            evaluationResult.setSegSpeed(rate);
+            result.add(evaluationResult);
         }
         //5、输出测试报告
-        LOGGER.info("*********************************************");
-        for(String key : map.keySet()){
-            LOGGER.info(key+" ：");
-            LOGGER.info(map.get(key));
+        LOGGER.info("**********************************************************************************************");
+        Collections.sort(result);
+        for(int i=0; i<result.size(); i++){
+            LOGGER.info(result.get(i).toString());
+            if(i < result.size()-1){
+                LOGGER.info("");
+            }
         }
-        LOGGER.info("*********************************************");
+        LOGGER.info("**********************************************************************************************");
     }
     /**
      * 生成测试数据集和标准数据集
      * @param file 已分词文本，词之间空格分隔
      * @param test 生成测试数据集文件路径
      * @param standard 生成标准数据集文件路径
+     * @return 测试数据集字符数
      */
-    public static void generateDataset(String file, String test, String standard){
+    public static int generateDataset(String file, String test, String standard){
+        int textCharCount=0;
         try(BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file),"utf-8"));
             BufferedWriter testWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(test),"utf-8"));
             BufferedWriter standardWriter = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(standard),"utf-8"))){
@@ -109,7 +119,9 @@ public class Evaluation {
                         continue;
                     }
                     set.add(item);
-                    testWriter.write(item.replaceAll(" ", "")+"\n");
+                    String testItem = item.replaceAll(" ", "");
+                    textCharCount += testItem.length();
+                    testWriter.write(testItem+"\n");
                     standardWriter.write(item+"\n");
                 }
             }
@@ -117,13 +129,14 @@ public class Evaluation {
         } catch (IOException ex) {
             LOGGER.error("生成测试数据集和标准数据集失败：", ex);
         }
+        return textCharCount;
     }
     /**
      * 分词效果评估
      * @param resultText 实际分词结果
      * @param standardText 标准分词结果
      */
-    private static String evaluation(String resultText, String standardText, String perfectResult, String wrongResult) {
+    private static EvaluationResult evaluation(String resultText, String standardText, String perfectResult, String wrongResult) {
         long start = System.currentTimeMillis();
         int perfectCount=0;
         int wrongCount=0;
@@ -160,9 +173,113 @@ public class Evaluation {
         int total = perfectCount+wrongCount;
         int totalCharCount = perfectCharCount+wrongCharCount;
         LOGGER.info("评估耗时："+cost+" 毫秒");
-        String report = "总行数："+total+"  ，完美行数："+perfectCount+"  ，错误行数："+wrongCount+"  ，完美率："+perfectCount/(float)total*100+"%  ，错误率："+wrongCount/(float)total*100+"%\n";
-        report+="总字符："+totalCharCount+" ，完美字符："+perfectCharCount+" ，错误行数："+wrongCharCount+" ，完美率："+perfectCharCount/(float)totalCharCount*100+"% ，错误率："+wrongCharCount/(float)totalCharCount*100+"%";
-        LOGGER.info(report);
-        return report;
+        EvaluationResult er = new EvaluationResult();
+        er.setPerfectCharCount(perfectCharCount);
+        er.setPerfectCount(perfectCount);
+        er.setTotalCharCount(totalCharCount);
+        er.setTotalLineCount(total);
+        er.setWrongCharCount(wrongCharCount);
+        er.setWrongCount(wrongCount);     
+        return er;
+    }
+    private static class EvaluationResult implements Comparable{
+        private SegmentationAlgorithm segmentationAlgorithm;
+        private float segSpeed;
+        private int totalLineCount;
+        private int perfectCount;
+        private int wrongCount;
+        private int totalCharCount;
+        private int perfectCharCount;
+        private int wrongCharCount;
+
+        public SegmentationAlgorithm getSegmentationAlgorithm() {
+            return segmentationAlgorithm;
+        }
+        public void setSegmentationAlgorithm(SegmentationAlgorithm segmentationAlgorithm) {
+            this.segmentationAlgorithm = segmentationAlgorithm;
+        }
+        public float getSegSpeed() {
+            return segSpeed;
+        }
+        public void setSegSpeed(float segSpeed) {
+            this.segSpeed = segSpeed;
+        }
+        public float getLinePerfectRate(){
+            return perfectCount/(float)totalLineCount*100;
+        }
+        public float getLineWrongRate(){
+            return wrongCount/(float)totalLineCount*100;
+        }
+        public float getCharPerfectRate(){
+            return perfectCharCount/(float)totalCharCount*100;
+        }
+        public float getCharWrongRate(){
+            return wrongCharCount/(float)totalCharCount*100;
+        }
+        public int getTotalLineCount() {
+            return totalLineCount;
+        }
+        public void setTotalLineCount(int totalLineCount) {
+            this.totalLineCount = totalLineCount;
+        }
+        public int getPerfectCount() {
+            return perfectCount;
+        }
+        public void setPerfectCount(int perfectCount) {
+            this.perfectCount = perfectCount;
+        }
+        public int getWrongCount() {
+            return wrongCount;
+        }
+        public void setWrongCount(int wrongCount) {
+            this.wrongCount = wrongCount;
+        }
+        public int getTotalCharCount() {
+            return totalCharCount;
+        }
+        public void setTotalCharCount(int totalCharCount) {
+            this.totalCharCount = totalCharCount;
+        }
+        public int getPerfectCharCount() {
+            return perfectCharCount;
+        }
+        public void setPerfectCharCount(int perfectCharCount) {
+            this.perfectCharCount = perfectCharCount;
+        }
+        public int getWrongCharCount() {
+            return wrongCharCount;
+        }
+        public void setWrongCharCount(int wrongCharCount) {
+            this.wrongCharCount = wrongCharCount;
+        }
+        @Override
+        public String toString(){
+            return segmentationAlgorithm.name()+"（"+segmentationAlgorithm.getDes()+"）："
+                    +"\n"
+                    +"分词速度："+segSpeed+" 字符/毫秒"
+                    +"\n"
+                    +"行数完美率："+getLinePerfectRate()+"%"
+                    +"  行数错误率："+getLineWrongRate()+"%"
+                    +"  总的行数："+totalLineCount
+                    +"  完美行数："+perfectCount
+                    +"  错误行数："+wrongCount
+                    +"\n"
+                    +"字数完美率："+getCharPerfectRate()+"%"
+                    +" 字数错误率："+getCharWrongRate()+"%"
+                    +" 总的字数："+totalCharCount
+                    +" 完美字数："+perfectCharCount
+                    +" 错误字数："+wrongCharCount;
+        }
+        @Override
+        public int compareTo(Object o) {
+            EvaluationResult other = (EvaluationResult)o;
+            if(other.getLinePerfectRate() - getLinePerfectRate() > 0){
+                return 1;
+            }
+            if(other.getLinePerfectRate() - getLinePerfectRate() < 0){
+                return -1;
+            }
+            return 0;
+        }
     }
 }
