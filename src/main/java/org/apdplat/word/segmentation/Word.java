ee745c0307cc537b01ab1b4b4ff9c233e3f86892
ysc <yang-shangchuan@qq.com>
2015-04-12 23:04:48 +0800

    新增词性、词频

--- src/main/java/org/apdplat/word/segmentation/Word.java
+++ src/main/java/org/apdplat/word/segmentation/Word.java
@@ -23,25 +23,54 @@ package org.apdplat.word.segmentation;
 import java.util.Objects;
 
 /**
- * 词
+ * 词、词性、词频
  * Word
  * @author 杨尚川
  */
-public class Word {
+public class Word implements Comparable{
     private String text;
+    private PartOfSpeech partOfSpeech = PartOfSpeech.UNKNOWN;
+    private int frequency;
+
     public Word(String text){
         this.text = text;
     }
+
+    public Word(String text, PartOfSpeech partOfSpeech, int frequency) {
+        this.text = text;
+        this.partOfSpeech = partOfSpeech;
+        this.frequency = frequency;
+    }
+
     public String getText() {
         return text;
     }
+
     public void setText(String text) {
         this.text = text;
-    }    
+    }
+
+    public PartOfSpeech getPartOfSpeech() {
+        return partOfSpeech;
+    }
+
+    public void setPartOfSpeech(PartOfSpeech partOfSpeech) {
+        this.partOfSpeech = partOfSpeech;
+    }
+
+    public int getFrequency() {
+        return frequency;
+    }
+
+    public void setFrequency(int frequency) {
+        this.frequency = frequency;
+    }
+
     @Override
     public int hashCode() {
         return Objects.hashCode(this.text);
     }
+
     @Override
     public boolean equals(Object obj) {
         if (obj == null) {
@@ -53,8 +82,30 @@ public class Word {
         final Word other = (Word) obj;
         return Objects.equals(this.text, other.text);
     }
+
     @Override
     public String toString(){
-        return text;
+        return text+" "+partOfSpeech+" "+frequency;
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        if(this == o){
+            return 0;
+        }
+        if(this.text == null){
+            return -1;
+        }
+        if(o == null){
+            return 1;
+        }
+        if(!(o instanceof Word)){
+            return 1;
+        }
+        String t = ((Word)o).getText();
+        if(t == null){
+            return 1;
+        }
+        return this.text.compareTo(t);
     }
 }
