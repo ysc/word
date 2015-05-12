353c184ac47a815eab5c468d09d2e468bccc75b6
ysc <yang-shangchuan@qq.com>
2015-05-12 18:30:05 +0800

    词典操作接口

--- src/main/java/org/apdplat/word/dictionary/Dictionary.java
+++ src/main/java/org/apdplat/word/dictionary/Dictionary.java
@@ -23,16 +23,60 @@ package org.apdplat.word.dictionary;
 import java.util.List;
 
 /**
- * 词典
+ * 词典操作接口
  * @author 杨尚川
  */
 public interface Dictionary {
+    /**
+     * 词典中的词的最大长度，即有多少个字符
+     * @return 长度
+     */
     public int getMaxLength();
+
+    /**
+     * 判断指定的文本是不是一个词
+     * @param item 文本
+     * @param start 指定的文本从哪个下标索引开始
+     * @param length 指定的文本的长度
+     * 比如：contains("我爱写程序",  3, 2);
+     * 表示的意思是“程序”是不是一个定义在词典中的词
+     * @return 是否
+     */
     public boolean contains(String item, int start, int length);
+
+    /**
+     * 判断文本是不是一个词
+     * @param item 文本
+     * @return 是否
+     */
     public boolean contains(String item);
+
+    /**
+     * 批量将词加入词典
+     * @param items 集合中的每一个元素是一个词
+     */
     public void addAll(List<String> items);
+
+    /**
+     * 将单个词加入词典
+     * @param item 词
+     */
     public void add(String item);
+
+    /**
+     * 批量将词从词典中删除
+     * @param items 集合中的每一个元素是一个词
+     */
     public void removeAll(List<String> items);
+
+    /**
+     * 将单个词从词典中删除
+     * @param item 词
+     */
     public void remove(String item);
+
+    /**
+     * 清空词典中的所有的词
+     */
     public void clear();
 }
