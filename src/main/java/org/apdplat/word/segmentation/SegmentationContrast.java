7f7f868b8a17964fb59af42643db6beb83a049c6
ysc <yang-shangchuan@qq.com>
2015-05-13 18:23:04 +0800

    对比各种分词算法的分词结果

--- src/main/java/org/apdplat/word/segmentation/SegmentationContrast.java
+++ src/main/java/org/apdplat/word/segmentation/SegmentationContrast.java
@@ -24,12 +24,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 /**
+ * 对比各种分词算法的分词结果
  * @author 杨尚川
  */
 public class SegmentationContrast {
