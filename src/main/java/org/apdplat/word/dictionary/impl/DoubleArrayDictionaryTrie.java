e35b01d845140797bc009f9e7283277aef19b53e
ysc <yang-shangchuan@qq.com>
2015-05-15 05:23:27 +0800

    代码改进

--- src/main/java/org/apdplat/word/dictionary/impl/DoubleArrayDictionaryTrie.java
+++ src/main/java/org/apdplat/word/dictionary/impl/DoubleArrayDictionaryTrie.java
@@ -100,12 +100,12 @@ public class DoubleArrayDictionaryTrie implements Dictionary{
 
         if (!siblings.isEmpty()) {
             siblings.get(siblings.size() - 1).right = parent.right;
-        }
-        if(LOGGER.isDebugEnabled()) {
-            LOGGER.debug("************************************************");
-            LOGGER.debug("树信息：");
-            siblings.forEach(s -> LOGGER.debug(s.toString()));
-            LOGGER.debug("************************************************");
+            if(LOGGER.isDebugEnabled()) {
+                LOGGER.debug("************************************************");
+                LOGGER.debug("树信息：");
+                siblings.forEach(s -> LOGGER.debug(s.toString()));
+                LOGGER.debug("************************************************");
+            }
         }
         return siblings;
     }
