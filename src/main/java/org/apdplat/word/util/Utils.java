ff99f20f2b65c2d52a913731206f6b63588fbebe
ysc <yang-shangchuan@qq.com>
2015-05-16 14:26:16 +0800

    删除不在需要的工具方法

--- src/main/java/org/apdplat/word/util/Utils.java
+++ src/main/java/org/apdplat/word/util/Utils.java
@@ -21,11 +21,6 @@
 package org.apdplat.word.util;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
 import java.util.regex.Pattern;
 
 /**
@@ -75,50 +70,5 @@ public class Utils {
             }
         }
         return dir.delete();
-    }
-    /**
-     * 根据MAP的VALUE进行排序
-     * @param <K> key
-     * @param <V> value
-     * @param map map
-     * @return 根据MAP的VALUE由大到小的排序结果列表
-     */
-    public static <K, V extends Number> List<Map.Entry<K, V>> getSortedMapByValue(Map<K, V> map) {        
-        List<Map.Entry<K, V>> list = new ArrayList<>(map.entrySet());  
-        Collections.sort(list, new Comparator<Map.Entry<K,V>>() {    
-            @Override
-            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {    
-                if(o1.getValue() instanceof Integer){
-                    return o2.getValue().intValue() - o1.getValue().intValue();
-                }
-                if(o1.getValue() instanceof Long){
-                    return (int)(o2.getValue().longValue() - o1.getValue().longValue());
-                }
-                if(o1.getValue() instanceof Float){
-                    float f1 = o1.getValue().floatValue();
-                    float f2 = o2.getValue().floatValue();
-                    if(f1 < f2){
-                        return 1;
-                    }
-                    if(f1 == f2){
-                        return 0;
-                    }
-                    return -1;
-                }
-                if(o1.getValue() instanceof Double){
-                    double f1 = o1.getValue().doubleValue();
-                    double f2 = o2.getValue().doubleValue();
-                    if(f1 < f2){
-                        return 1;
-                    }
-                    if(f1 == f2){
-                        return 0;
-                    }
-                    return -1;
-                }
-                return 0;
-            }    
-        });     
-        return list;  
     }
 }
