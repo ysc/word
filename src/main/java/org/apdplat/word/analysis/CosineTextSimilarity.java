1b3483408213d1c140280873712ecba123bf7786
ysc <yang-shangchuan@qq.com>
2015-05-20 03:39:57 +0800

    余弦相似度

---
+++ src/main/java/org/apdplat/word/analysis/CosineTextSimilarity.java
@@ -0,0 +1,101 @@
+/**
+ *
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.apdplat.word.analysis;
+
+import org.apdplat.word.segmentation.Word;
+
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 文本相似度计算
+ * 判定方式：余弦相似度，通过计算两个向量的夹角余弦值来评估他们的相似度
+ * @author 杨尚川
+ */
+public class CosineTextSimilarity extends TextSimilarity {
+    /**
+     * 判定相似度的方式：余弦相似度
+     * 余弦夹角原理：
+     * 向量a=(x1,y1),向量b=(x2,y2)
+     * a.b=x1x2+y1y2
+     * |a|=根号[(x1)^2+(y1)^2],|b|=根号[(x2)^2+(y2)^2]
+     * @param frequency1 文本1的词频统计结果
+     * @param frequency2 文本2的词频统计结果
+     * @return 相似度分值
+     */
+    @Override
+    protected double scoreImpl(Map<Word, AtomicInteger> frequency1, Map<Word, AtomicInteger> frequency2) {
+        Set<Word> words = new HashSet<>();
+        words.addAll(frequency1.keySet());
+        words.addAll(frequency2.keySet());
+        //向量的维度为words的大小，每一个维度的权重是词频
+        //a.b
+        AtomicInteger ab = new AtomicInteger();
+        //|a|
+        AtomicInteger aa = new AtomicInteger();
+        //|b|
+        AtomicInteger bb = new AtomicInteger();
+        //计算
+        words
+            .stream()
+            .forEach(word -> {
+                AtomicInteger x1 = frequency1.get(word);
+                AtomicInteger x2 = frequency2.get(word);
+                if (x1 != null && x2 != null) {
+                    //x1x2
+                    int oneOfTheDimension = x1.get() * x2.get();
+                    //+
+                    ab.addAndGet(oneOfTheDimension);
+                }
+                if (x1 != null) {
+                    //(x1)^2
+                    int oneOfTheDimension = x1.get() * x1.get();
+                    //+
+                    aa.addAndGet(oneOfTheDimension);
+                }
+                if (x2 != null) {
+                    //(x2)^2
+                    int oneOfTheDimension = x2.get() * x2.get();
+                    //+
+                    bb.addAndGet(oneOfTheDimension);
+                }
+            });
+
+        double aaa = Math.sqrt(aa.get());
+        double bbb = Math.sqrt(bb.get());
+        //使用BigDecimal保证精确计算浮点数
+        BigDecimal aabb = BigDecimal.valueOf(aaa).multiply(BigDecimal.valueOf(bbb));
+        double cos = ab.get()/aabb.doubleValue();
+        return cos;
+    }
+
+    public static void main(String[] args) {
+        String text1 = "我爱学习";
+        String text2 = "我爱读书";
+        TextSimilarity textSimilarity = new CosineTextSimilarity();
+        System.out.println(text1+" 和 "+text1+" 的相似度分值："+textSimilarity.similarScore(text1, text1));
+        System.out.println(text1+" 和 "+text2+" 的相似度分值："+textSimilarity.similarScore(text1, text2));
+        System.out.println(text2+" 和 "+text2+" 的相似度分值："+textSimilarity.similarScore(text2, text2));
+    }
+}
