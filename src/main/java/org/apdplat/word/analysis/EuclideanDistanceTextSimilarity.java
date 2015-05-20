94c6e566899e0eb1e89bba8bfc74ca848a840390
ysc <yang-shangchuan@qq.com>
2015-05-21 07:36:38 +0800

    欧几里得距离

---
+++ src/main/java/org/apdplat/word/analysis/EuclideanDistanceTextSimilarity.java
@@ -0,0 +1,115 @@
+/**
+ *
+ * APDPlat - Application Product Development Platform
+ * Copyright (c) 2013, 杨尚川, yang-shangchuan@qq.com
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package org.apdplat.word.analysis;
+
+import org.apdplat.word.segmentation.Word;
+
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 文本相似度计算
+ * 判定方式：欧几里得距离（Euclidean Distance），通过计算两点间的距离来评估他们的相似度
+ * 欧几里得距离原理：
+ * 设A(x1, y1)，B(x2, y2)是平面上任意两点
+ * 两点间的距离dist(A,B)=sqrt((x1-x2)^2+(y1-y2)^2)
+ * @author 杨尚川
+ */
+public class EuclideanDistanceTextSimilarity extends TextSimilarity {
+    /**
+     * 判定相似度的方式：欧几里得距离
+     * 欧几里得距离原理：
+     * 设A(x1, y1)，B(x2, y2)是平面上任意两点
+     * 两点间的距离dist(A,B)=sqrt((x1-x2)^2+(y1-y2)^2)
+     * @param words1 词列表1
+     * @param words2 词列表2
+     * @return 相似度分值
+     */
+    @Override
+    protected double scoreImpl(List<Word> words1, List<Word> words2) {
+        //词频统计
+        Map<Word, AtomicInteger> frequency1 = frequency(words1);
+        Map<Word, AtomicInteger> frequency2 = frequency(words2);
+        //输出词频统计信息
+        if(LOGGER.isDebugEnabled()){
+            LOGGER.debug("词频统计1：\n{}", formatWordsFrequency(frequency1));
+            LOGGER.debug("词频统计2：\n{}", formatWordsFrequency(frequency2));
+        }
+        //所有的不重复词
+        Set<Word> words = new HashSet<>();
+        words.addAll(words1);
+        words.addAll(words2);
+        //向量的维度为words的大小，每一个维度的权重是词频
+        //(x1-x2)^2+(y1-y2)^2
+        AtomicInteger ab = new AtomicInteger();
+        //计算
+        words
+            .parallelStream()
+            .forEach(word -> {
+                AtomicInteger x1 = frequency1.get(word);
+                AtomicInteger x2 = frequency2.get(word);
+                if (x1 == null) {
+                    x1 = new AtomicInteger(0);
+                }
+                if (x2 == null) {
+                    x2 = new AtomicInteger(0);
+                }
+                //(x1-x2)
+                int oneOfTheDimension = x1.get() - x2.get();
+                //(x1-x2)^2
+                //+
+                ab.addAndGet(oneOfTheDimension * oneOfTheDimension);
+            });
+        //distance=sqrt((x1-x2)^2+(y1-y2)^2)
+        double euclideanDistance = Math.sqrt(ab.get());
+        double score = 0;
+        if(euclideanDistance == 0){
+            //距离为0，表示完全相同
+            score = 1;
+        }else {
+            score = 1 / euclideanDistance;
+        }
+        if(LOGGER.isDebugEnabled()){
+            LOGGER.debug("文本1和文本2的欧几里得距离："+euclideanDistance);
+            LOGGER.debug("文本1和文本2的相似度分值：1 / "+euclideanDistance+"="+score);
+        }
+        return score;
+    }
+
+    public static void main(String[] args) {
+        String text1 = "我爱购物";
+        String text2 = "我爱读书";
+        String text3 = "他是黑客";
+        TextSimilarity textSimilarity = new EuclideanDistanceTextSimilarity();
+        double score1pk1 = textSimilarity.similarScore(text1, text1);
+        double score1pk2 = textSimilarity.similarScore(text1, text2);
+        double score1pk3 = textSimilarity.similarScore(text1, text3);
+        double score2pk2 = textSimilarity.similarScore(text2, text2);
+        double score2pk3 = textSimilarity.similarScore(text2, text3);
+        double score3pk3 = textSimilarity.similarScore(text3, text3);
+        System.out.println(text1+" 和 "+text1+" 的相似度分值："+score1pk1);
+        System.out.println(text1+" 和 "+text2+" 的相似度分值："+score1pk2);
+        System.out.println(text1+" 和 "+text3+" 的相似度分值："+score1pk3);
+        System.out.println(text2+" 和 "+text2+" 的相似度分值："+score2pk2);
+        System.out.println(text2+" 和 "+text3+" 的相似度分值："+score2pk3);
+        System.out.println(text3+" 和 "+text3+" 的相似度分值："+score3pk3);
+    }
+}
