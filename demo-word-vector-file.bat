93e97ff28176ddaf37abcc1e040c3178bc2b7ddc
ysc <yang-shangchuan@qq.com>
2015-05-22 07:51:44 +0800

    更改数据保存目录

--- demo-word-vector-file.bat
+++ demo-word-vector-file.bat
@@ -7,16 +7,16 @@ call mvn -Dmaven.test.skip clean install dependency:copy-dependencies
 set JAVA_OPTS=-Xms12000m -Xmx12000m
 set CLASS_PATH=target/classes;target/dependency/slf4j-api-1.6.4.jar;target/dependency/logback-classic-0.9.28.jar;target/dependency/logback-core-0.9.28.jar
 set EXECUTOR=java %JAVA_OPTS% -cp %CLASS_PATH%
-rem 1、对文件%TEXT_FILE%进行分词，词之间以空格分隔，内容保存到文件target/word.txt
-call %EXECUTOR% org.apdplat.word.WordSegmenter file %TEXT_FILE% target/word.txt
-rem 2、对分好词的文件target/word.txt建立词向量，将词向量保存到文件target/vector.txt，将词汇表保存到文件target/vocabulary.txt
+rem 1、对文件%TEXT_FILE%进行分词，词之间以空格分隔，内容保存到文件 data/word.txt
+call %EXECUTOR% org.apdplat.word.WordSegmenter file %TEXT_FILE%  data/word.txt
+rem 2、对分好词的文件 data/word.txt建立词向量，将词向量保存到文件 data/vector.txt，将词汇表保存到文件 data/vocabulary.txt
 rem 上下文长度为
 set CONTEXT_WINDOW_LENGTH=2
 rem 词向量长度
 set VECTOR_LENGTH=30
-call %EXECUTOR% org.apdplat.word.vector.Word2Vector target/word.txt target/vector.txt target/vocabulary.txt %CONTEXT_WINDOW_LENGTH% %VECTOR_LENGTH%
+call %EXECUTOR% org.apdplat.word.vector.Word2Vector  data/word.txt  data/vector.txt  data/vocabulary.txt %CONTEXT_WINDOW_LENGTH% %VECTOR_LENGTH%
 rem 3、计算不同词向量之间的相似度，控制台编码为GBK
-call %EXECUTOR% org.apdplat.word.vector.Distance target/vector.txt gbk
+call %EXECUTOR% org.apdplat.word.vector.Distance  data/vector.txt gbk
 
 :END
 echo 文本文件%TEXT_FILE%不存在
--- demo-word-vector-file.sh
+++ demo-word-vector-file.sh
@@ -15,13 +15,13 @@ mvn -Dmaven.test.skip clean install dependency:copy-dependencies
 export JAVA_OPTS="-Xms12000m -Xmx12000m"
 export CLASS_PATH="target/classes:target/dependency/slf4j-api-1.6.4.jar:target/dependency/logback-classic-0.9.28.jar:target/dependency/logback-core-0.9.28.jar"
 export EXECUTOR="java $JAVA_OPTS -cp $CLASS_PATH"
-# 1、对文件$TEXT_FILE进行分词，词之间以空格分隔，内容保存到文件target/word.txt
-$EXECUTOR org.apdplat.word.WordSegmenter file $TEXT_FILE target/word.txt
-# 2、对分好词的文件target/word.txt建立词向量，将词向量保存到文件target/vector.txt，将词汇表保存到文件target/vocabulary.txt
+# 1、对文件$TEXT_FILE进行分词，词之间以空格分隔，内容保存到文件 data/word.txt
+$EXECUTOR org.apdplat.word.WordSegmenter file $TEXT_FILE  data/word.txt
+# 2、对分好词的文件 data/word.txt建立词向量，将词向量保存到文件 data/vector.txt，将词汇表保存到文件 data/vocabulary.txt
 # 上下文长度为
 export CONTEXT_WINDOW_LENGTH=2
 # 词向量长度
 export VECTOR_LENGTH=30
-$EXECUTOR org.apdplat.word.vector.Word2Vector target/word.txt target/vector.txt target/vocabulary.txt $CONTEXT_WINDOW_LENGTH $VECTOR_LENGTH
+$EXECUTOR org.apdplat.word.vector.Word2Vector  data/word.txt  data/vector.txt  data/vocabulary.txt $CONTEXT_WINDOW_LENGTH $VECTOR_LENGTH
 # 3、计算不同词向量之间的相似度，控制台编码为UTF-8
-$EXECUTOR org.apdplat.word.vector.Distance target/vector.txt utf-8
+$EXECUTOR org.apdplat.word.vector.Distance  data/vector.txt utf-8
