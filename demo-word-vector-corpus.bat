5536cd70cb04516016598debe43f71f0144fa633
ysc <yang-shangchuan@qq.com>
2015-05-22 09:50:07 +0800

    优化计算过程

--- demo-word-vector-corpus.bat
+++ demo-word-vector-corpus.bat
@@ -4,12 +4,12 @@ set JAVA_OPTS=-Xms12000m -Xmx12000m
 set CLASS_PATH=target/classes;target/dependency/slf4j-api-1.6.4.jar;target/dependency/logback-classic-0.9.28.jar;target/dependency/logback-core-0.9.28.jar
 set EXECUTOR=java %JAVA_OPTS% -cp %CLASS_PATH%
 rem 1、从语料库中提取词，词之间以空格分隔，内容保存到文件 data/word.txt
-call %EXECUTOR% org.apdplat.word.corpus.ExtractText  data/word.txt
+if not exist data/word.txt call %EXECUTOR% org.apdplat.word.corpus.ExtractText  data/word.txt
 rem 2、对分好词的文件 data/word.txt建立词向量，将词向量保存到文件 data/vector.txt，将词汇表保存到文件 data/vocabulary.txt
 rem 上下文长度为
 set CONTEXT_WINDOW_LENGTH=2
 rem 词向量长度
 set VECTOR_LENGTH=30
-call %EXECUTOR% org.apdplat.word.vector.Word2Vector  data/word.txt  data/vector.txt  data/vocabulary.txt %CONTEXT_WINDOW_LENGTH% %VECTOR_LENGTH%
+if not exist data/vector.txt call %EXECUTOR% org.apdplat.word.vector.Word2Vector  data/word.txt  data/vector.txt  data/vocabulary.txt %CONTEXT_WINDOW_LENGTH% %VECTOR_LENGTH%
 rem 3、计算不同词向量之间的相似度，控制台编码为GBK
 call %EXECUTOR% org.apdplat.word.vector.Distance  data/vector.txt gbk
--- demo-word-vector-corpus.sh
+++ demo-word-vector-corpus.sh
@@ -4,12 +4,16 @@ export JAVA_OPTS="-Xms12000m -Xmx12000m"
 export CLASS_PATH="target/classes:target/dependency/slf4j-api-1.6.4.jar:target/dependency/logback-classic-0.9.28.jar:target/dependency/logback-core-0.9.28.jar"
 export EXECUTOR="java $JAVA_OPTS -cp $CLASS_PATH"
 # 1、从语料库中提取词，词之间以空格分隔，内容保存到文件 data/word.txt
+if [ ! -f data/word.txt ]; then
 $EXECUTOR org.apdplat.word.corpus.ExtractText data/word.txt
+fi
 # 2、对分好词的文件 data/word.txt 建立词向量，将词向量保存到文件 data/vector.txt，将词汇表保存到文件 data/vocabulary.txt
 # 上下文长度为
+if [ ! -f data/vector.txt ]; then
 export CONTEXT_WINDOW_LENGTH=2
 # 词向量长度
 export VECTOR_LENGTH=30
 $EXECUTOR org.apdplat.word.vector.Word2Vector data/word.txt data/vector.txt data/vocabulary.txt $CONTEXT_WINDOW_LENGTH $VECTOR_LENGTH
+fi
 # 3、计算不同词向量之间的相似度，控制台编码为UTF-8
 $EXECUTOR org.apdplat.word.vector.Distance data/vector.txt utf-8
